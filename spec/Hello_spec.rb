c5bf525266fcc423c28c369dd907d3b4291ebc27
Matheus dos Santos Silva <matheus.santos.x4@gmail.com>
2021-02-12 10:35:23 -0300

    Correção da tag page.title

--- spec/Hello_spec.rb
+++ spec/Hello_spec.rb
@@ -2,7 +2,7 @@ describe "Meu primeiro Script" do
         it "visitar a pagina" do
         puts "Abrindo o Navegador e acessando a Pagina..."
         visit "https://www.dimepkairos.com.br/"
-        titulo = (page.titulo)
+        titulo = (page.title)
         sleep = 5
         expect(titulo).to eql "kairos"
     end
