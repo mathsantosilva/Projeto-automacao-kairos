054b70c9c1bf4ae391b878d18d6d4c2e2c80f383
Matheus dos Santos Silva <matheus.santos.x4@gmail.com>
2021-02-22 14:31:03 -0300

    Adicionado teste de login, (Sucesso, falha e usuario não cadastrado)

--- spec/forms_spec.rb
+++ spec/forms_spec.rb
@@ -1,11 +1,24 @@
 describe 'Acessando o Kairos', :Teste1 do
-    it 'realizando login na pagina' do
+    it 'realizando login com sucesso' do
         visit 'https://www.dimepkairos.com.br/Dimep/Account/LogOn?ReturnUrl=%2F'
-
         fill_in 'LogOnModel_UserName', with: 'mat_santos21@hotmail.com'
-        fill_in 'LogOnModel_Password', with: '1'
+        fill_in 'LogOnModel_Password', with: 'teste'
         click_button 'btnFormLogin'
-        expect(find("div[class='TabMenuNomeEmpresa shortText']")).to have_content 'Web Café - Alterar a Razão Social - Alteração de nome'
+
+        expect(find('div.TabMenuWrapper').visible?).to be true
+        expect(find('div.TabMenuWrapper')).to have_content "Web Café - Alterar a Razão Social - Alteração de nome"
+        sleep 3
+        
+    end
+    it 'Realizando login com falha' do
+        visit 'https://www.dimepkairos.com.br/Dimep/Account/LogOn?ReturnUrl=%2F'
+        fill_in 'LogOnModel_UserName', with: 'mat_santos21@hotmail.com'
+        fill_in 'LogOnModel_Password', with: '123'
+        click_button 'btnFormLogin'
+
+        expect(find("div[class='field-validation-error']").visible?).to be true
+        expect(find("div[class='field-validation-error']")).to have_content 'Usuário e/ou senha estão incorretos.'
         sleep 3
     end
+
 end
