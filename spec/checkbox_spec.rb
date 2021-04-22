2fa35d1b4dee74fd96fcd4ea01484fe3b561e2ae
Matheus dos Santos Silva <matheus.santos.x4@gmail.com>
2021-04-22 15:49:28 -0300

    Adicionado a função de adicionar um grupo ao usuario / adicionado ao after a validação da mensagem de sucesso

--- spec/checkbox_spec.rb
+++ spec/checkbox_spec.rb
@@ -7,25 +7,39 @@ describe 'Selecionando usuarios', :checkbox do
         visit '/Dimep/Usuarios'
     end
 
-    it 'marcando o mat_santos' do
+    it 'marcando o mat_santos', :usuario1 do
         find('label[for="2"]', :visible=>false).set(true)
         email = find('table[id="Grid2"]')
         expect(email).to have_content 'mat_santos21@hotmail.com'
+        find('input[id="AlterarGrupo"]').click
+        select('TI', from: 'select-grupo')
+        find('span[id="SaveVincularGrupo"]').click
+
     end
 
-    it 'marcando o junior.cesar' do
+    it 'marcando o junior.cesar', :usuario2 do
         find('label[for="5"]', :visible=>false).set(true)
         email = find('table[id="Grid2"]')
         expect(email).to have_content 'junior.cesar@swlmaq.com.br'
+        find('input[id="AlterarGrupo"]').click
+        select('Funcionarios', from: 'select-grupo')
+        find('span[id="SaveVincularGrupo"]').click
+
     end
 
-    it 'marcando o gaucho' do
+    it 'marcando o gaucho', :usuario3 do
         find('label[for="6"]', :visible=>false).set(true)
         email = find('table[id="Grid2"]')
         expect(email).to have_content 'gaucho@dimep.com.br'
+        find('input[id="AlterarGrupo"]').click
+        select('Gestores', from: 'select-grupo')
+        find('span[id="SaveVincularGrupo"]').click
+
     end
 
     after(:each) do
+        mensagem = find('div[class="validation-summary-ok"]')
+        expect(mensagem).to have_content 'Vinculação de grupo alterada com sucesso.'
         sleep 3
     end
 end
